3d71e03178bcadec0d0cd6483dc8bc9b943acae5
bajway <kashifbaj54321@gmail.com>
2024-09-05 23:34:51 +0500

    Update Slope (Question 6).c

--- Slope (Question 6).c
+++ Slope (Question 6).c
@@ -1,9 +1,12 @@
+//program to find the slope
 #include<stdio.h>
 int main(){
+    //Declaring variables
     int X1, X2, Y1, Y2;
     X1=5; X2=3; Y1=4; Y2=2;
     float Slope;
+    //Formula to calculate slope
     Slope= (Y2-Y1)/(X2-X1);
     printf("The slope of the line is %.3f", Slope);
     return 0;
-}
+}// End main
